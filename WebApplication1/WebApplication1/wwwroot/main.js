a89bd6f81fce0c9f4795bc9e1ef8943979201968
Mehmet Can Onur Yaralı <mco2hy@gmail.com>
2019-11-26 20:00:00 +0300

    Prime project

---
+++ WebApplication1/WebApplication1/wwwroot/main.js
@@ -0,0 +1,13 @@
+﻿function buttonClick() {
+    alert("Düğmeye basıldı.");
+}
+
+var buttonClick2 = function() {
+    alert("button2 basıldı!");
+}
+
+
+var string = "xhers";
+var number = 123.3;
+var object = [1, 2, 3[a, b, c[undefined[1[2[3[[[null["asd123"[[1]]]]]]]]]]]];
+
